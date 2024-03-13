bc7a10f4c8a46e7aac7eb06d2b2f547ee182de4b
rezaaditria <rezaaditria1@gmail.com>
2024-03-13 12:29:00 +0700

    fix build fail

--- src/components/features/Dashboard.js
+++ src/components/features/Dashboard.js
@@ -1,7 +1,7 @@
 import styles from './Dashboard.module.css'
-import React, { useState, useEffect } from "react";
-import Chart from "react-apexcharts";
-import { fetchData } from '../../API/fetch';
+import React from "react";
+// import Chart from "react-apexcharts";
+// import { fetchData } from '../../API/fetch';
 import Piechart from '../Piechart'
 import Barchart from '../Barchart'
 import Areachart from '../Areachart'
@@ -9,45 +9,45 @@ import Heatmap from '../Heatmap'
 
 
 function App() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const fetchedData = await fetchData();
+  //     setData(fetchedData);
+  //   };
+    
+  //   getData();
+  // }, []);
+  
+  // const divisi = [...new Set(data.map(item => item.divisi))];
+  // const seriesData = divisi.map(divisi => data.filter(item => item.divisi === divisi).length);
 
-  useEffect(() => {
-    const getData = async () => {
-      const fetchedData = await fetchData();
-      setData(fetchedData);
-    };
-
-    getData();
-  }, []);
-
-  const divisi = [...new Set(data.map(item => item.divisi))];
-  const seriesData = divisi.map(divisi => data.filter(item => item.divisi === divisi).length);
-
-  const options = {
-    chart: {
-      type: "bar",
-      stacked: false,
-      zoom: { enabled: false },
-      toolbar: {
-        show: false,
-      },
-    },
-    plotOptions: {
-      bar: {
-        horizontal: true,
-        barHeight: "80%",
-      },
-    },
-    colors: ["#875BF7"],
-    stroke: { show: true, width: 1 },
-    fill: { opacity: 1 },
-    dataLabels: {
-      enabled: false,
-    },
-    xaxis: {
-      categories: divisi,
-    },
-  };
+  // const options = {
+  //   chart: {
+  //     type: "bar",
+  //     stacked: false,
+  //     zoom: { enabled: false },
+  //     toolbar: {
+  //       show: false,
+  //     },
+  //   },
+  //   plotOptions: {
+  //     bar: {
+  //       horizontal: true,
+  //       barHeight: "80%",
+  //     },
+  //   },
+  //   colors: ["#875BF7"],
+  //   stroke: { show: true, width: 1 },
+  //   fill: { opacity: 1 },
+  //   dataLabels: {
+  //     enabled: false,
+  //   },
+  //   xaxis: {
+  //     categories: divisi,
+  //   },
+  // };
 
 
 
@@ -55,9 +55,7 @@ function App() {
   <div className={styles.App}>
     <div className={styles.row}>
       <div >
-      <Piechart 
-      width={"30%"}
-      options={{ maintainAspectRatio: false }}/>
+      <Piechart />
       </div>
       <div>
       <Barchart/>
